a0898b864939a49f3e4f451a6dd52be8e61d02a6
pamalj098 <pamaljayasinghe340@gmail.com>
2025-05-23 23:59:23 +0530

    new update

--- fibads/components/Hero/Hero.js
+++ fibads/components/Hero/Hero.js
@@ -21,15 +21,15 @@ const Hero = () => {
     <svg
       className={className}
       style={{ transform: `rotate(${rotation}deg)` }}
-      width="70"
-      height="70"
+      width="80"
+      height="80"
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
         d="M12 1L15.5 8.5H23L17 13.5L19 22L12 17.5L5 22L7 13.5L1 8.5H8.5L12 1Z"
-        fill="white"
+        fill="#c5ff00"
       />
     </svg>
   );
@@ -41,22 +41,37 @@ const Hero = () => {
           <div className="hero-content">
             <div className="agency-location">
               <span className="location-text">
-                Creative Agency Beside New York USA
+                Creative Agency Beside
+                <span className="location-arrow">→</span>
+                <br />
+                New York USA
               </span>
             </div>
+          </div>
+        </div>
 
-            <div className="creative-title-container">
-              <div className="title-row">
-                <h1 className="hero-title first-title">CREATIVE</h1>
-                <StarIcon className="rotating-star" />
-              </div>
-              <div className="title-row">
-                <h1 className="hero-title large-title">BRANDING</h1>
-                <span className="smiley-icon">☺</span>
-              </div>
-              <div className="title-row">
-                <h1 className="hero-title last-title">WORLD.</h1>
-              </div>
+        <div className="hero-center-column">
+          <div className="creative-title-container">
+            <div className="title-row">
+              <h1 className="hero-title first-title">CRETIVE</h1>
+              <StarIcon className="rotating-star" />
+            </div>
+            <div className="title-row">
+              <h1 className="hero-title large-title">BRANDING</h1>
+              <span className="smiley-icon">😊</span>
+            </div>
+            <div className="title-row">
+              <h1 className="hero-title last-title">
+                W
+                <span className="o-letter">
+                  <img
+                    src="/img/3d-ring.png"
+                    alt="3D O"
+                    className="o-image"
+                  />
+                </span>
+                RLD.
+              </h1>
             </div>
           </div>
         </div>
@@ -72,6 +87,14 @@ const Hero = () => {
             </p>
           </div>
         </div>
+      </div>
+
+      <div className="wireframe-sphere">
+        <img
+          src="/img/wireframe-sphere.png"
+          alt="Wireframe Sphere"
+          className="sphere-image"
+        />
       </div>
     </section>
   );
